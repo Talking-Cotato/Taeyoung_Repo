9d0e823e804a48e447ed68f92b815871cafb3421
sharpie1330 <amberaplin14@gmail.com>
2023-11-21 20:31:52 +0900

    BOJ 1676번 팩토리얼 0의 개수 (개선)

--- BOJ/BOJ_1676.java
+++ BOJ/BOJ_1676.java
@@ -9,18 +9,11 @@ public class BOJ_1676 {
 
         int N = sc.nextInt();
 
-        Stack<Integer> stack10 = new Stack<>();
         Stack<Integer> stack5 = new Stack<>();
         Stack<Integer> stack2 = new Stack<>();
 
         for (int i = N; i > 0; i--) {
             int temp = i;
-            if (temp % 10 == 0) {
-                while (temp % 10 == 0) {
-                    temp = temp / 10;
-                    stack10.add(10);
-                }
-            }
             if (temp % 5 == 0) {
                 while (temp % 5 == 0) {
                     temp = temp / 5;
@@ -35,11 +28,9 @@ public class BOJ_1676 {
             }
         }
 
-        int cnt10 = stack10.toArray().length;
         int cnt5 = stack5.toArray().length;
         int cnt2 = stack2.toArray().length;
 
-        int zeroCnt = cnt10 + Math.min(cnt2, cnt5);
-        System.out.println(zeroCnt);
+        System.out.println(Math.min(cnt2, cnt5));
     }
 }
